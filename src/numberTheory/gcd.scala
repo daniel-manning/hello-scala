ee1884fd064df0d7844aba6442278e5f5fa18fb7
ecastilla95 <ecastilla95@gmail.com>
2018-10-28 09:13:19 +0100

    Some number theory

---
+++ src/numberTheory/gcd.scala
@@ -0,0 +1,20 @@
+package numberTheory
+
+object gcd {
+
+  def gcd(x: Int, y: Int): Int =
+  {
+    if (x == y) x
+    else if (x > y) gcd(x-y, y)
+    else gcd(y-x, x)
+  }
+
+  def acceptInputAndComputeGCD(pair:List[Int]): Unit = {
+    println(gcd(pair.head,pair.reverse.head))
+  }
+
+  def main(args: Array[String]) {
+    acceptInputAndComputeGCD(readLine().trim().split(" ").map(x=>x.toInt).toList)
+
+  }
+}
