c5db57ee77226a30b45c48a35366f9e7b1836268
enrique.castilla <enrique.castilla@pragsis.com>
2019-06-03 16:52:14 +0200

    Filter Elements

---
+++ src/hackerrank/FilterElements.scala
@@ -0,0 +1,19 @@
+package hackerrank
+
+object FilterElements {
+
+  def main(args: Array[String]) {
+    val stdin = scala.io.StdIn
+    for(_ <- 0 until stdin.readInt){
+      def readArray: Array[Int] = stdin.readLine.trim.split(' ').map(_.toInt)
+
+      val args = readArray
+      val array = readArray
+
+      val map: Map[Int, Int] = array.groupBy(identity).mapValues(_.length)
+      val result = array.distinct.filter(map(_) >= args(1))
+
+      println(if (result.isEmpty) "-1" else result.mkString(" "))
+    }
+  }
+}
