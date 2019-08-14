84d06b4ebe064923a7ee517bc38562b3cd605d37
enrique.castilla <enrique.castilla@pragsis.com>
2019-08-14 13:35:44 +0200

    Rotate String

---
+++ src/hackerrank/RotateString.scala
@@ -0,0 +1,23 @@
+package hackerrank
+
+object RotateString {
+
+  def main(args: Array[String]) {
+
+    val stdIn = scala.io.StdIn
+
+    def rotate(s: String): String = s.tail + s.head
+
+    def rotateNTimes(s: String, n: Int): String = if (n <= 0) s else rotateNTimes(rotate(s), n-1)
+
+    def getRotations(s: String) = {
+      (1 to s.length).map( n => rotateNTimes(s, n)).mkString(" ")
+    }
+
+    val T = stdIn.readInt()
+    for (_ <- 1 to T) {
+      val line = stdIn.readLine().trim
+      println(getRotations(line))
+    }
+  }
+}
